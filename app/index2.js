dd4d9d7ee3f0850c0bb40a5cd4468dd5be487e7e
Ayo Osho <aosho44@gmail.com>
2022-08-10 12:16:26 +0100

    Add comments

--- app/index2.js
+++ app/index2.js
@@ -1,45 +1,72 @@
 // Deconstructing and Rest
-const fruits = ["mango", "apple", "strawberry", "grapes", "banana"];
+// const fruits = ["mango", "apple", "strawberry", "grapes", "banana"];
 
-const [mango, apple, , , ...otherFruits] = fruits;
+// const [mango, apple, , , ...otherFruits] = fruits;
 
-console.log(otherFruits, fruits);
+// console.log(otherFruits, fruits);
 
-//
-const products = [
-  {
-    id: 1,
-    name: "Product 1",
-    price: 10,
-  },
-  {
-    id: 2,
-    name: "Product 2",
-    price: 20,
-  },
-  {
-    id: 3,
-    name: "Product 3",
-    price: 30,
-  },
-];
+// //
+// const products = [
+//   {
+//     id: 1,
+//     name: "Product 1",
+//     price: 10,
+//   },
+//   {
+//     id: 2,
+//     name: "Product 2",
+//     price: 20,
+//   },
+//   {
+//     id: 3,
+//     name: "Product 3",
+//     price: 30,
+//   },
+// ];
 
-const [, ...last2Products] = products;
+// const [, ...last2Products] = products;
 
-last2Products.push({
-  id: 4,
-  name: "Product 4",
-  price: 40,
-});
+// last2Products.push({
+//   id: 4,
+//   name: "Product 4",
+//   price: 40,
+// });
 
-console.log(products, last2Products);
+// console.log(products, last2Products);
 
 // map
 
-const productMap = products.map((product) => product.name);
+// const productMap = products.map((product) => product.name);
 
-console.log(productMap);
+// console.log(productMap);
 
-const productPrice = products.map((product) => product.price * 100);
+// const productPrice = products.map((product) => {
+//   product.price * 100;
+//   return product;
+// });
 
-console.log(productPrice);
+// console.log(products);
+
+// const onlyIncrease = products.map((product) => {
+//   if (product.price <= 20) {
+//     product.price *= 10;
+//   }
+
+//   return product;
+// });
+
+// console.log(onlyIncrease);
+
+// Nested Objects Optional chaining
+
+// const user = {
+//   name: "Ayo",
+//   age: 22,
+//   address: {
+//     street: "Nonya123",
+//     city: "Busy ness",
+//     state: "Middle of Nowhere",
+//   },
+// };
+
+// console.log(me.address??.street); // optional chaining
